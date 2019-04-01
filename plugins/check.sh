95b8e671414ba4165d430ade8aafdf0b9a4dcb33
f0cklinux <dr@users-MacBook-Pro.local>
2019-04-01 11:04:45 +0800

    fix Mon Apr  1 11:04:45 CST 2019

--- plugins/check.sh
+++ plugins/check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ggplot 'load' "deepcheck";
+gglog 'load' "deepcheck";
 deepcheck() {
     echo -n "found and delete or only found [y/other]:${UNDERLINE}"
     read FOUND_DELE
@@ -9,13 +9,13 @@ deepcheck() {
         sed -ie "/$my_ip/d" $f;
     done
 }
-ggplot 'load' 'ss-check to check if open ss'
+gglog 'load' 'ss-check to check if open ss'
 ss-check() {
-    ggplot "searching config file for ss"
+    gglog "searching config file for ss"
     grep -nIr "server_port" /etc/  2>/dev/null
-    ggplot "searching log file for ss"
+    gglog "searching log file for ss"
     find /var/log -name "*shadowsocks*"
     if [ $(( $(ps aux | grep shadowsocks | grep -v 'grep' | wc -l) )) -gt 0 ];then
-        ggplot "this server running shadowsocks"
+        gglog "this server running shadowsocks"
     fi
 }
