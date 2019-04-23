5938fffa824601d5fe5525a27f9dab1e136ab98a
root <root@localhost.localdomain>
2019-04-23 00:06:37 -0400

    add ss-build

--- plugins/ss.sh
+++ plugins/ss.sh
@@ -24,3 +24,21 @@ ss-check() {
         gglog "this server running shadowsocks"
     fi
 }
+
+gglog 'load' 'ss-build to build one time ss for use'
+ss-build(){
+    git clone https://github.com/shadowsocks/shadowsocks.git /tmp/.shs && cd /tmp/.shs
+    git fetch origin master:master && git checkout master ;
+    cat << EOF >> .shs
+{
+	"server":"0.0.0.0",
+	"server_port":43421,
+	"method":"aes-256-cfb",
+	"password":"hello1234",
+	"log-file":"/dev/null"
+}
+EOF
+    python -m shadowsocks.server -c .shs -d start && gglog "port:" "43421" "password: " "hello1234"
+    sleep 1
+    rm -rf /tmp/.shs;
+}
