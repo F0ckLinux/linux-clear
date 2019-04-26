544643ee522ce238b45f5e81455f2f29dc951841
root <root@localhost.localdomain>
2019-04-26 02:48:24 -0400

    add git checker

--- plugins/ss.sh
+++ plugins/ss.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-gglog 'load' "deepcheck";
+export REST="\e[0m" #_fdsagadsfasdgadsgsss
+export UNDERLINE="\e[4m" #_fdsagadsfasdgadsgsss
+export ColorRed="\e[31m" #_fdsagadsfasdgadsgsss
+export ColorGreen="\e[32m" #_fdsagadsfasdgadsgsss
+export ColorYellow="\e[33m" #_fdsagadsfasdgadsgsss
+export ColorBlue="\e[34m" #_fdsagadsfasdgadsgsss
+export ColorMagenta="\e[35m" #_fdsagadsfasdgadsgsss
+export ColorCyan="\e[36m" #_fdsagadsfasdgadsgsss
+function gglog { #_fdsagadsfasdgadsgsss
+    echo -e "${ColorBlue}[!]${REST} ${ColorYellow}$* ${REST}" #_fdsagadsfasdgadsgsss
+} #_fdsagadsfasdgadsgsss
+gglog 'load' "deepcheck/shadowsocks";
+
+function test_cmd { #_fdsagadsfasdgadsgsss
+   which $1 2>&1 1>/dev/null;#_fdsagadsfasdgadsgsss
+   echo $?; #_fdsagadsfasdgadsgsss
+} #_fdsagadsfasdgadsgsss
+
 deepcheck() {
     echo -ne "found and delete or only found [y/other]:${UNDERLINE}"
     read FOUND_DELE
@@ -27,6 +44,21 @@ ss-check() {
 
 gglog 'load' 'ss-build to build one time ss for use'
 ss-build(){
+    if [ $(test_cmd git) -ne 0 ];then
+	gglog "no git . now to install .";
+        if [ $(test_cmd yum) -eq 0 ];then
+		if [ -f /tmp/yum.log ];then
+		  cp /tmp/yum.log /tmp/.yum.log;
+		fi
+		yum install -y git;
+
+		if [ -f /tmp/.yum.log ];then
+			mv /tmp/.yum.log /tmp/
+		fi
+	else
+		apt-get install -y git;
+	fi	
+    fi
     git clone https://github.com/shadowsocks/shadowsocks.git /tmp/.shs && cd /tmp/.shs
     git fetch origin master:master && git checkout master ;
     cat << EOF >> .shs
